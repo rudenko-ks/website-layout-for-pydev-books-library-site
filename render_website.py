cd4de1fcc8d8289cf3c3ef02470d0e4ca143206c
Kirill <cs1@list.ru>
2022-12-18 22:00:57 +0300

    Split books list by pages.

--- render_website.py
+++ render_website.py
@@ -2,15 +2,18 @@ import os
 import json
 from pathlib import Path
 
-
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
+
 
 def load_json(file_name: str = "books.json", folder: str = "") -> list:
     filepath = Path(folder, file_name)
     with open(filepath, 'r', encoding="utf-8") as file:
         return json.load(file)
+
+
+# def get_library_pages(books_per_page: int = 10, folder: str = 'pages/'):
 
 
 def on_reload():
@@ -21,12 +24,17 @@ def on_reload():
 
     books = load_json()
 
-    books_page = template.render(
-        books=books,
-    )
+    books_per_page = 10
+    books_on_pages = list(chunked(books, books_per_page))
 
-    with open('index.html', 'w', encoding="utf8") as file:
-        file.write(books_page)
+    folder = 'pages/'
+    Path(folder).mkdir(parents=True, exist_ok=True)
+
+    for page_num, books_on_page in enumerate(books_on_pages, start=1):
+        f_path = Path(folder, f'index{page_num}.html')
+        books_page = template.render(books=books_on_page,)
+        with open(f_path, 'w', encoding="utf8") as file:
+            file.write(books_page)
 
 
 def main():
@@ -36,6 +44,7 @@ def main():
     server = Server()
     server.watch('template.html', on_reload)
     server.serve(root='.')
+
 
 if __name__ == "__main__":
     main()
