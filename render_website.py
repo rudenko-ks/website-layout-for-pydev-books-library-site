c9670fdb6dcf8cb6ab430d701fefa35718d3ef2e
Kirill <cs1@list.ru>
2022-12-29 16:23:02 +0300

    аllowed to specify a different path to the file

--- render_website.py
+++ render_website.py
@@ -1,25 +1,46 @@
+import argparse
 import os
 import json
 from pathlib import Path
+import sys
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from livereload import Server
 from more_itertools import chunked
 
 
-def get_book_descriptions_from_file(file_name: str = "books.json", folder: str = "library/") -> list:
-    filepath = Path(folder, file_name)
+def create_argparser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description="""\
+        Скрипт представляет собой пример создания библиотеки научно-фантастических книг.
+        В карточках книг на странице будут собраны данные в виде заголовков, автора,
+        текста книг в формате .txt и обложек страниц при их наличии.""",
+                                     formatter_class=argparse.RawTextHelpFormatter)
+    parser.add_argument(
+        '--filepath',
+        help='Путь к JSON-файлу с данными о книгах. По умолчанию: library/books.json',
+        type=str,
+        default='library/books.json')
+    return parser.parse_args()
+
+
+def get_book_descriptions_from_file(filepath: str) -> list:
     with open(filepath, 'r', encoding="utf-8") as file:
         return json.load(file)
 
 
 def on_reload():
+    args = create_argparser()
+
+    if not Path(args.filepath).is_file():
+        print('Указанный файл не найден.')
+        sys.exit(0)
+
     env = Environment(loader=FileSystemLoader('.'),
                       autoescape=select_autoescape(['html', 'xml']))
 
     template = env.get_template('/templates/template.html')
 
-    books = get_book_descriptions_from_file()
+    books = get_book_descriptions_from_file(args.filepath)
 
     books_per_page = 16
     books_on_pages = list(chunked(books, books_per_page))
